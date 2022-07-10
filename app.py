4b927db0da7b85ff1b289e6063c7d0d785cd405c
Doan Anh Tien <d.atien228@gmail.com>
2022-07-10 23:14:18 +0700

    add samesite header to response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 from modules.model import summarize
 import __main__
 
@@ -18,14 +18,17 @@ def home():
 def recommend():
     if request.method == "POST":
         # Get form data
-        request_data = request.get_json()
-        input_text = request_data['input_text']
+        # request_data = request.args.get("input").get_json()
+        # input_text = request_data['input_text']
+        request_data = request.args.get("input")
+        input_text = request_data.get_json()['input_text']
 
         # Call the function summarize to run the text summarization
         try:
             short_output_summary, long_output_summary = summarize(input_text)
-            response = jsonify({'short': short_output_summary.strip(), 'long': long_output_summary.strip()})
-            response.headers.add('Access-Control-Allow-Origin', '*')
+            response = make_response(jsonify({'short': short_output_summary.strip(), 'long': long_output_summary.strip()}))
+            response.set_cookie('cookie1', 'value1', samesite='Lax')
+            response.set_cookie('cookie2', 'value2', samesite='None', secure=True)
             # Pass output summary to the output template
             return response
 
